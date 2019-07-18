8d673c65eeec5cd2472f04b8b3c2f480551ecf28
Jeff Day <jeffrey.day00@gmail.com>
2019-07-17 17:46:51 -0700

    Types in TypeScript.

---
+++ app.js
@@ -0,0 +1,10 @@
+// string
+var myName = "heff";
+//myName = 28;
+// number
+var myAge = 28.5;
+//booleans
+var hasHobbies = true;
+// hasHobbies = 'basketball'
+var myRealAge;
+myRealAge = 28;
--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log("It works!");
+// string
+let myName = "heff"
+
+//myName = 28;
+
+// number
+let myAge = 28.5;
+
+//booleans
+let hasHobbies = true;
+// hasHobbies = 'basketball'
+
+let myRealAge: number;
+myRealAge = 28;
